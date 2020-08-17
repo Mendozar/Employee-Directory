29f7090f3953e69aef5b2504e8329015cb9c7d7a
hoffman1200 <hoffman1200@hotmail.com>
2020-08-16 23:37:36 -0500

    Axios Working

---
+++ employee-directory/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import Titles from "./components/Titles";
+import Search from "./components/Search";
+
+
+function App() {
+  return(
+      <div className="flexbox-container">
+        <Navbar />
+        <Titles/>
+        <Search/>
+      </div>
+    
+
+  
+  )
+}
+
+export default App;
