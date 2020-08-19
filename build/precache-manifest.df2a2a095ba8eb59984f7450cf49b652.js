737fdb3e41a5503398bc8bbcc45c758a8c9bcc40
hoffman1200 <hoffman1200@hotmail.com>
2020-08-18 22:03:00 -0500

    Change Repo Name

---
+++ build/precache-manifest.df2a2a095ba8eb59984f7450cf49b652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c8b41fe8e935ef3c2cbdc4b0a4c2b8",
+    "url": "/hoffman1200/Homework-19/index.html"
+  },
+  {
+    "revision": "ef5599b13b3e4b8594c2",
+    "url": "/hoffman1200/Homework-19/static/css/main.03bd0d11.chunk.css"
+  },
+  {
+    "revision": "dfcf0dc96d1724c0c9ff",
+    "url": "/hoffman1200/Homework-19/static/js/2.7f0e3670.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/hoffman1200/Homework-19/static/js/2.7f0e3670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5599b13b3e4b8594c2",
+    "url": "/hoffman1200/Homework-19/static/js/main.9122c1c6.chunk.js"
+  },
+  {
+    "revision": "798ac74f85a6aca3942b",
+    "url": "/hoffman1200/Homework-19/static/js/runtime-main.aaf084d6.js"
+  }
+]);
